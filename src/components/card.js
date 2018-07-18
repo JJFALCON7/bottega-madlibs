b9a11cb7435f0fdb86e1f7a31ada62038cbb3b30
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-18 11:10:45 -0600

    added the styles

--- src/components/card.js
+++ src/components/card.js
@@ -4,30 +4,34 @@ import Input from './input';
 
 import Content from './content';
 
+const INITIAL_STATE = {
+    color: '',
+    pluralNoun: '',
+    adjectiveOne: '',
+    celebOne: '',
+    adjectiveTwo: '',
+    nounOne: '',
+    numberOne:'',
+    numberTwo: '',
+    nounTwo: '',
+    adjectiveThree: '',
+    celebTwo: '',
+    celebThree: '',
+    adjectiveFour: '',
+    nounThree: '',
+    celebFour: '',
+    adjectiveFive: '',
+    contentVisible: false
+}
+
 class Card extends Component {
 
 constructor() {
     super()
 
-    this.state = {
-        color: '',
-        pluralNoun: '',
-        adjectiveOne: '',
-        celebOne: '',
-        adjectiveTwo: '',
-        nounOne: '',
-        numberOne:'',
-        numberTwo: '',
-        nounTwo: '',
-        adjectiveThree: '',
-        celebTwo: '',
-        celebThree: '',
-        adjectiveFour: '',
-        nounThree: '',
-        celebFour: '',
-        adjectiveFive: '',
-        contentVisible: false
-    }
+    this.state = INITIAL_STATE;
+        
+    
     
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -39,7 +43,14 @@ handleInputChange(event) {
 
 handleFormSubmit(event) {  
 event.preventDefault()
-this.setState({contentVisible: !this.state.contentVisible})
+
+if(this.state.contentVisible){
+    this.setState(INITIAL_STATE)
+}else {
+    this.setState({contentVisible: true}) 
+}
+
+
 }
 
 
@@ -74,7 +85,9 @@ this.setState({contentVisible: !this.state.contentVisible})
             <div className="card__inputs">
             
             {
-                inputData.map(data => Input( (data), this.handleInputChange ))
+                inputData.map((data, index) => { 
+                   return Input( (data), this.handleInputChange, index  )
+                })
             }
             </div>
             <button type="submit">{!this.state.contentVisible ? 'Generate Mad lib' : 'Clear Form'}</button>
